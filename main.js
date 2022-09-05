2900c708566710b0eb54a8ce75c4d2bc00fa020a
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-09-05 16:46:58 +0600

    Use Strict

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict"; // Can check your code's silly mistake and find the error location.
+
+nickName();
+function nickName(){
+    name = "Nayeem";
+}
+
+console.log(nickName());
+
+// without "use strict"; can't check the code's silly mistake and find the error location.
+fullName();
+function fullName(){
+    name = "Abu Md Nayeem Khan";
+}
